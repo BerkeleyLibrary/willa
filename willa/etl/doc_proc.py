91683c6f33a7ba5505429c758d69122ef6f21dfa
A. Wilcox <AWilcox@Wilcox-Tech.com>
2025-08-08 20:47:28 -0500

    ETL: Add methods to fetch directly from TIND
    Implements: AP-392

--- willa/etl/doc_proc.py
+++ willa/etl/doc_proc.py
@@ -10,11 +10,37 @@ from operator import add
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 # from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
 from langchain_core.vectorstores.base import VectorStore
+from pymarc.record import Record
+
 import willa.config  # pylint: disable=W0611
+from willa.tind.format_validate_pymarc import pymarc_to_metadata
 
 
-def load_pdfs() -> list:
+def load_pdf(name: str, record: Record | None) -> list[Document]:
+    """Load a given single PDF from storage, including optional PyMARC record.
+
+    :param str name: The name of the file.
+    :param record: The PyMARC record that pertains to the file.
+    :returns: A ``list`` of ``Document``s that can be further used in the pipeline.
+    """
+    directory_path = os.getenv('DEFAULT_STORAGE_DIR', 'tmp/files/')
+    loader = PyPDFDirectoryLoader(directory_path, glob=name, mode="single")
+
+    docs = loader.load()
+    if not docs:
+        print(f"Requested file {name} not found.")
+    else:
+        print(f"Loaded {name} as a document.")
+        if record:
+            for doc in docs:
+                doc.metadata['tind_metadata'] = pymarc_to_metadata(record)
+
+    return docs
+
+
+def load_pdfs() -> list[Document]:
     """Load PDF files from a specified directory using a langchain loader."""
     directory_path = os.getenv('DEFAULT_STORAGE_DIR', 'tmp/files/')
     loader = PyPDFDirectoryLoader(directory_path, mode="single")
@@ -28,7 +54,7 @@ def load_pdfs() -> list:
     return docs
 
 
-def split_doc(doc: dict, chunk_size: int = 1000, chunk_overlap: int = 200) -> list:
+def split_doc(doc: Document, chunk_size: int = 1000, chunk_overlap: int = 200) -> list:
     """Split a document into chunks for vectorization."""
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
