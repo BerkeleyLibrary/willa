BerkeleyLibrary/willa

commit 4120982d2d4a9611e87ac83f4abe614eec4d8126

tind: Add fetch_file API to fetch a record's files
Implements: AP-365

willa/tind/fetch.py
@@ -2,11 +2,13 @@
 Provides routines to fetch information from the TIND API.
 """
 
+import os
+
 from io import StringIO
 from pymarc.marcxml import parse_xml_to_array
 from pymarc import Record
 from willa.errors import RecordNotFoundError
-from .api import tind_get
+from .api import tind_get, tind_download
 
 
 def fetch_metadata(record: str) -> Record:
@@ -22,7 +24,35 @@ def fetch_metadata(record: str) -> Record:
         raise RecordNotFoundError(f"Record {record} not found in TIND.")
 
     records = parse_xml_to_array(StringIO(response))
-    if len(records) > 1:
-        raise RecordNotFoundError(f"Record {record} matched more than one record in TIND.")
+    if len(records) != 1:
+        raise RecordNotFoundError(f"Record {record} did not match exactly one record in TIND.")
 
     return records[0]
+
+
+def fetch_file(file_url: str, output_dir: str = ''):
+    """Fetch the given file from TIND.
+
+    :param str file_url: The URL to the file to download from TIND.
+                         This must be a TIND file download URL.
+    :param str output_dir: The directory in which to save the file.
+    :raises AuthorizationError: When the TIND API key is invalid, or the file is restricted.
+    :raises ValueError: When the URL is not a valid TIND file download URL.
+    :raises RecordNotFoundError: When the file is invalid or not found.
+    :raises IOError: When the file cannot be saved to the given output directory.
+    :returns: When the file has been successfully downloaded to the output directory.
+    """
+    if not file_url.endswith('/download/'):
+        raise ValueError('URL is not a valid TIND file download URL.')
+
+    if output_dir == '':
+        # This cannot be put as the default value for the parameter because it would set
+        # the value at the time this file is imported.  Changes in the environment (whether
+        # via config reload, the test infrastructure, etc) would not appear.
+        output_dir = os.getenv('DEFAULT_STORAGE_DIR')
+
+    output_filename = file_url.split('/')[-3]
+    status = tind_download(file_url, os.path.join(output_dir, output_filename))
+
+    if status != 200:
+        raise RecordNotFoundError(f"Referenced file {output_filename} could not be downloaded.")

tests/tind/test_fetch.py
@@ -3,10 +3,12 @@ Test the TIND fetch record functionality of Willa.
 """
 
 import os
+import pathlib
+import tempfile
 import unittest
 
 import requests_mock
-from willa.errors import RecordNotFoundError
+from willa.errors import AuthorizationError, RecordNotFoundError
 from willa.tind import fetch
 
 
@@ -41,3 +43,77 @@ class TindFetchMetadataTest(unittest.TestCase):
             r_mock.get('https://digicoll.lib.berkeley.edu/api/v1/record/99999999/',
                        text=' \n')
             self.assertRaises(RecordNotFoundError, fetch.fetch_metadata, '99999999')
+
+
+class TindFetchFileTest(unittest.TestCase):
+    """Test the fetch_file method of the willa.tind.fetch module."""
+    def setUp(self):
+        os.environ['TIND_API_KEY'] = 'Test_Key'
+        os.environ['DEFAULT_STORAGE_DIR'] = tempfile.mkdtemp(prefix='willatest')
+
+    def test_file_fetch(self):
+        """Test a simple file fetch."""
+        dl_path = 'https://ucb.tind.example/api/v1/record/1234/files/test.txt/download/'
+        expected = 'Hello world, from Python\n'
+
+        with requests_mock.mock() as r_mock:
+            r_mock.get(dl_path, text=expected)
+            fetch.fetch_file(dl_path)
+
+        path = pathlib.Path(os.getenv('DEFAULT_STORAGE_DIR'), 'test.txt')
+        self.assertTrue(path.is_file(), 'File should be saved to default path')
+        self.assertEqual(path.read_text(encoding='utf-8'), expected,
+                         'File should have expected contents')
+        path.unlink()
+
+    def test_fetch_to_custom_path(self):
+        """Test a file fetch to a custom path instead of the default."""
+        with tempfile.TemporaryDirectory(prefix='willa2') as custom_path:
+            dl_path = 'https://ucb.tind.example/api/v1/record/1234/files/custom.txt/download/'
+            expected = 'Fetched to a custom path\n'
+
+            with requests_mock.mock() as r_mock:
+                r_mock.get(dl_path, text=expected)
+                fetch.fetch_file(dl_path, custom_path)
+
+            path = pathlib.Path(os.getenv('DEFAULT_STORAGE_DIR'), 'custom.txt')
+            self.assertFalse(path.is_file(), 'File should not be saved in default path')
+
+            path = pathlib.Path(custom_path, 'custom.txt')
+            self.assertTrue(path.is_file(), 'File should be saved in custom path')
+            self.assertEqual(path.read_text(encoding='utf-8'), expected,
+                             'File should have expected contents')
+            path.unlink()
+
+    def test_invalid_url(self):
+        """Ensure an error is raised when an invalid URL is specified."""
+        self.assertRaises(ValueError, fetch.fetch_file, 'https://www.freebsd.org/')
+
+    def test_missing_file(self):
+        """Ensure an error is raised when a file does not exist."""
+        dl_path = 'https://ucb.tind.example/api/v1/record/1234/files/nothere.txt/download/'
+        with requests_mock.mock() as r_mock:
+            r_mock.get(dl_path, status_code=404)
+            self.assertRaises(RecordNotFoundError, fetch.fetch_file, dl_path)
+
+    def test_write_error(self):
+        """Ensure an error is raised when attempting to write a file to an invalid location."""
+        dl_path = 'https://ucb.tind.example/api/v1/record/1234/files/error.txt/download/'
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            # At the end of this block, the directory will be removed.
+            custom_path = temp_dir
+
+        with requests_mock.mock() as r_mock:
+            r_mock.get(dl_path, text='This file will not be saved.\n')
+            self.assertRaises(IOError, fetch.fetch_file, dl_path, custom_path)
+
+    def test_insufficient_perm(self):
+        """Ensure an error is raised when a file cannot be accessed by the given API key."""
+        dl_path = 'https://ucb.tind.example/api/v1/record/1234/files/restricted.txt/download/'
+        with requests_mock.mock() as r_mock:
+            r_mock.get(dl_path, status_code=401)
+            self.assertRaises(AuthorizationError, fetch.fetch_file, dl_path)
+
+    def tearDown(self):
+        pathlib.Path(os.environ['DEFAULT_STORAGE_DIR']).rmdir()

willa/tind/api.py
@@ -9,6 +9,10 @@ import willa.config  # pylint: disable=W0611
 from willa.errors import AuthorizationError
 
 
+TIMEOUT: int = 30
+"""The number of seconds to wait for an HTTP connection to respond."""
+
+
 def _auth_header() -> dict:
     """Returns the Authorization header needed for TIND API calls."""
     token = os.getenv('TIND_API_KEY', None)
@@ -30,9 +34,33 @@ def tind_get(endpoint: str, params: dict = None) -> (int, str):
     if params is None:
         params = {}
 
-    resp = requests.request('GET',
-                            f"https://digicoll.lib.berkeley.edu/api/v1/{endpoint}",
-                            headers=_auth_header(), params=params, timeout=30)
+    resp = requests.get(f"https://digicoll.lib.berkeley.edu/api/v1/{endpoint}",
+                        headers=_auth_header(), params=params, timeout=TIMEOUT)
     if resp.status_code == 401:
         raise AuthorizationError('Invalid TIND API key provided')
+    if resp.status_code >= 500:
+        resp.raise_for_status()
     return resp.status_code, resp.text
+
+
+def tind_download(url: str, output_path: str) -> int:
+    """Download a file from TIND.
+
+    :param str url: The TIND file download URL.
+    :param str output_path: The full path in which to save the file, including name.
+    :returns: The HTTP status code.
+    """
+    resp = requests.get(url, headers=_auth_header(), timeout=TIMEOUT)
+    status = resp.status_code
+    if status == 401:
+        raise AuthorizationError('Invalid TIND API key provided')
+    if status >= 500:
+        resp.raise_for_status()
+    if status != 200:
+        return status
+
+    with open(output_path, 'wb') as out_f:
+        for chunk in resp.iter_content():
+            out_f.write(chunk)
+
+    return status

commit 0461470b820e02496b70b4e6464039df4b00d69a

tind: Handle TIND-provided download URLs
Some contain '?version=...' at the end.

Fixes: 4120982d2d ("tind: Add fetch_file API to fetch a record's files")

willa/tind/fetch.py
@@ -3,6 +3,7 @@ Provides routines to fetch information from the TIND API.
 """
 
 import os
+import re
 from io import StringIO
 from typing import Any, List, Tuple
 import json
@@ -50,7 +51,7 @@ def fetch_file(file_url: str, output_dir: str = '') -> str:
     :raises IOError: When the file cannot be saved to the given output directory.
     :returns str: The full path to the file successfully downloaded to the output directory.
     """
-    if not file_url.endswith('/download/'):
+    if not re.match(r'^http.*/download(/)?(\?version=\d+)?$', file_url):
         raise ValueError('URL is not a valid TIND file download URL.')
 
     if output_dir == '':