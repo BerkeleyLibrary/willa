55f6a58ffdff4bf4267b280c8aaa1e5db8116a69
David Zuckerman <dzuckerm@library.berkeley.edu>
2025-08-21 19:43:22 -0700

    First stab at having chat display history

--- willa/web/app.py
+++ willa/web/app.py
@@ -25,59 +25,32 @@ BOT = Chatbot(STORE, ChatOllama(model=os.getenv('CHAT_MODEL', 'gemma3n:e4b'),
                                 base_url=OLLAMA_URL))
 """The Chatbot instance to use for chatting."""
 
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    if (username, password) == ("admin", "admin"):
+        return cl.User(
+            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
+        )
+    else:
+        return None
+
+@cl.on_chat_resume
+async def on_chat_resume(thread):
+    pass
+
+@cl.step(type="tool")
+async def tool(message: cl.Message) -> str:
+    reply = await cl.make_async(BOT.ask)(message.content)
+    return reply 
 
 @cl.on_message
-async def chat(message: cl.Message) -> None:
+async def main(message: cl.Message) -> None:
     """Handle an incoming chat message."""
-    reply = await cl.make_async(BOT.ask)(message.content)
-
-    # enter user query and reply into db?
+    tool_res = await tool(message) 
 
     await cl.Message(
         author='Willa',
-        content=reply
+        content=tool_res
     ).send()
-
-# @cl.password_auth_callback
-# def auth_callback(username: str, password: str):
-#     # Fetch the user matching username from your database
-#     # and compare the hashed password with the value stored in the database
-#     if (username, password) == ("admin", "admin"):
-#         return cl.User(
-#             identifier="admin", metadata={"role": "admin", "provider": "credentials"}
-#         )
-#     else:
-#         return None
-
-# @cl.on_chat_resume
-# async def on_chat_resume(thread):
-#     pass
-
-
-# @cl.step(type="tool")
-# async def tool():
-#     # Fake tool
-#     await cl.sleep(2)
-#     return "Response from the tool!"
-
-
-# @cl.on_message  # this function will be called every time a user inputs a message in the UI
-# async def main(message: cl.Message):
-#     """
-#     This function is called every time a user inputs a message in the UI.
-#     It sends back an intermediate response from the tool, followed by the final answer.
-
-#     Args:
-#         message: The user's message.
-
-#     Returns:
-#         None.
-#     """
-#         message: The user's message.
-
-#     Returns:
-#         None.
-#     """
-
-
-
