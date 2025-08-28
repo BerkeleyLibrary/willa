613b0bca3944a934f29912cbe3651e213b29e822
A. Wilcox <AWilcox@Wilcox-Tech.com>
2025-08-27 19:51:39 -0500

    tests: Fix type issue in test_get_tind_url
    We do actually accept ints, it seems, but the function signature is to
    only accept str, so pass a str.

--- tests/tind/test_format_tind_context.py
+++ tests/tind/test_format_tind_context.py
@@ -18,24 +18,22 @@ class TindFormatValidatePymarc(unittest.TestCase):
     """Tests the output of the willa.tind.format_validate_pymarc module."""
 
     def setUp(self) -> None:
-        """Get a pymarc record for testing"""
+        """Get a PyMARC record for testing."""
 
         marc_xml = setup_files.setup_text_file('example_billups.xml')
         self.pymarc_records = parse_xml_to_array(StringIO(marc_xml))
         self.tind_dict = format_validate_pymarc.pymarc_to_metadata(self.pymarc_records[0])
 
-
     def test_process_fields(self) -> None:
-        """Test that Tind record is parsed with formatted fields""" 
+        """Test that Tind record is parsed with formatted fields."""
 
         result = format_tind_context.process_fields(self.tind_dict)
 
         self.assertIn("Contributor: Meeker, Martin interviewer.\n", result)
         self.assertIn("Contributor: Billups, Richard,  1943- interviewee.\n", result)
 
-
     def test_format_tind_context(self) -> None:
-        """Chatbot document with associated Tind Metadata should return formatted Tind data"""  
+        """Chatbot document with associated Tind Metadata should return formatted Tind data."""
 
         docs = load_pdf(os.path.join(os.path.dirname(__file__), 'billups_rich.pdf'),
                         self.pymarc_records[0])
@@ -46,7 +44,7 @@ class TindFormatValidatePymarc(unittest.TestCase):
                       tind_context)
 
     def test_get_tind_url(self) -> None:
-        """A Tind ID should return a Url to a Tind record"""
+        """A Tind ID should return a URL to a Tind record."""
 
-        tind_url = format_tind_context.get_tind_url(103508)
+        tind_url = format_tind_context.get_tind_url('103508')
         self.assertEqual('https://digicoll.lib.berkeley.edu/record/103508', tind_url)
