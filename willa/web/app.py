4cde4a33eea27162b76cda20a6a6ea999226ecfa
Jason Raitz <145712254+jason-raitz@users.noreply.github.com>
2025-09-08 16:16:51 -0400

    adding langgraph and persisting older conversations (#36)
    * adding langgraph and persisting older conversations
     - use chainlit data_layer to instantiate each chatbot instance
     - correctly passes previous conversations to chatbot
    
    * pylint changes
    
    * mypy changes
    
    * line too long
    
    * langgraph Chatbot optimization (#38)
    
    * Improvements to chatbot workflow graph
    
    * adds tind_metadata back to the generated response
    
    * fixes for mypy
    There was one case explicitly ignored by mypy. When invoke is called on the workflow, it expects a state type "StateT", but it will not accept any casting or reworking of the partial state dict to satisfy this check
    
    * more mypy and pylint changes
     - moved an info line to
    
    * restore previous chat process
    
    * alter vector store query structure
    
    * Update willa/chatbot/chatbot.py
    
    Co-authored-by: A. Wilcox <AWilcox@Wilcox-Tech.com>
    
    * top level logger on chatbot
    
    * Remove TODO comment about TIND metadata as system message
    
    * Simplify call to `cl.Message`
    
    * Add docstrings and style fixes
    
    ---------
    
    Co-authored-by: A. Wilcox <AWilcox@Wilcox-Tech.com>
    Co-authored-by: maría a. matienzo <73732+anarchivist@users.noreply.github.com>

--- willa/web/app.py
+++ willa/web/app.py
@@ -17,14 +17,11 @@ from willa.web.inject_custom_auth import add_custom_oauth_provider
 STORE = get_lance()
 """The vector store."""
 
+_THREAD_BOTS = {}
+"""The Chatbot instances associated with each thread."""
+
 
 add_custom_oauth_provider('cas', CASProvider())
-
-
-BOT = Chatbot(STORE, ChatOllama(model=os.getenv('CHAT_MODEL', 'gemma3n:e4b'),
-                                temperature=float(os.getenv('CHAT_TEMPERATURE', '0.5')),
-                                base_url=OLLAMA_URL))
-"""The Chatbot instance to use for chatting."""
 
 COMMANDS: list[CommandDict] = [
   {
@@ -56,6 +53,23 @@ def _get_history() -> str:
 
     return content
 
+def _get_or_create_bot(thread_id: str) -> Chatbot:
+    """Get or create a bot instance for the given thread."""
+    previous_conversation = cl.chat_context.to_openai()
+    if thread_id not in _THREAD_BOTS:
+        _THREAD_BOTS[thread_id] = Chatbot(
+            STORE,
+            ChatOllama(
+                model=os.getenv('CHAT_MODEL', 'gemma3n:e4b'),
+                temperature=float(os.getenv('CHAT_TEMPERATURE', '0.5')),
+                base_url=OLLAMA_URL
+            ),
+            thread_id=thread_id,
+            conversation_thread=previous_conversation
+        )
+
+    return _THREAD_BOTS[thread_id]
+
 @cl.on_message
 async def chat(message: cl.Message) -> None:
     """Handle an incoming chat message."""
@@ -69,11 +83,11 @@ async def chat(message: cl.Message) -> None:
           content="Transcript copied to clipboard!"
         ).send()
     else:
-        reply = await cl.make_async(BOT.ask)(message.content)
-        await cl.Message(
-          author='System',
-          content=reply
-        ).send()
+        # Use thread-specific
+        bot = _get_or_create_bot(message.thread_id)
+
+        reply = await cl.make_async(bot.ask)(message.content)
+        await cl.Message(content=reply).send()
 
 # Chainlit erroneously defines the callback as taking an `id_token` param that is never passed.
 @cl.oauth_callback  # type: ignore[arg-type]
