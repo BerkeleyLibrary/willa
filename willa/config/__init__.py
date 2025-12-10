BerkeleyLibrary/willa

commit 312bf52cdad617ac2cb030a1e33f96ed33d43db6

web: Add a custom data layer retrieval method
In local development (Docker Compose / .env), we have a ``DATABASE_URL``
variable that is set.  We do not have that in staging/prod Swarm stacks.
This causes the Chainlit default Data Layer init method to return None,
because it doesn't know how to connect to the database.

This introduces a small wrapper using the documented ``data_layer``
callback, which will use ``DATABASE_URL`` if present, but fall back to
the Postgres environment variables we use in the Swarm.

Closes: AP-530

willa/web/app.py
@@ -3,8 +3,10 @@ Implementation of the Web interface for Willa.
 """
 
 import logging
+import os
 
 import chainlit as cl
+from chainlit.data.chainlit_data_layer import ChainlitDataLayer
 from chainlit.types import ThreadDict, CommandDict
 
 from willa.chatbot import Chatbot
@@ -44,6 +46,21 @@ async def on_chat_resume(thread: ThreadDict) -> None:
     """Resume chat session for data persistence."""
     await cl.context.emitter.set_commands(COMMANDS)
 # pylint: enable="unused-argument"
+
+
+@cl.data_layer
+def data_layer() -> ChainlitDataLayer:
+    """Retrieve the data layer to use with Chainlit.
+
+    Simple wrapper around the native one that uses our ``POSTGRES_*`` env vars.
+    """
+    def _pg(var: str) -> str:
+        return os.environ[f'POSTGRES_{var}']
+
+    database_url = os.environ.get(
+        'DATABASE_URL', f"postgresql://{_pg('USER')}:{_pg('PASSWORD')}@{_pg('HOST')}/{_pg('DB')}"
+    )
+    return ChainlitDataLayer(database_url=database_url)
 
 
 def _get_history() -> str:

commit 17aff8a3d4c1f1689f97c873819f225cc04141b3

web: Read Postgres secret from Docker secrets
This fixes an issue where the Postgres password was not found in the
environment in Docker Swarm.

We look for a ``POSTGRES_PASSWORD_FILE``, and fallback to a hardcoded
``/run/secrets`` path if we don't find it.

Fixes: 312bf52cda ("web: Add a custom data layer retrieval method")
Ref: AP-530

willa/config/__init__.py
@@ -111,7 +111,7 @@ else:
 
 _NEEDS_ENVIRON: list[str] = ['AWS_DEFAULT_REGION', 'AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY',
                              'LANGFUSE_HOST', 'LANGFUSE_PUBLIC_KEY', 'LANGFUSE_SECRET_KEY',
-                             'CHAINLIT_AUTH_SECRET']
+                             'CHAINLIT_AUTH_SECRET', 'POSTGRES_PASSWORD_FILE']
 """A list of configuration keys that need to be set in the environment as well."""
 
 for key in _NEEDS_ENVIRON:

willa/web/app.py
@@ -57,8 +57,16 @@ def data_layer() -> ChainlitDataLayer:
     def _pg(var: str) -> str:
         return os.environ[f'POSTGRES_{var}']
 
+    def _secret() -> str:
+        if 'POSTGRES_PASSWORD' in os.environ:
+            return os.environ['POSTGRES_PASSWORD']
+
+        with open(os.environ.get('POSTGRES_PASSWORD_FILE',
+                                 '/run/secrets/POSTGRES_PASSWORD'), 'r', encoding='utf8') as p_file:
+            return p_file.read()
+
     database_url = os.environ.get(
-        'DATABASE_URL', f"postgresql://{_pg('USER')}:{_pg('PASSWORD')}@{_pg('HOST')}/{_pg('DB')}"
+        'DATABASE_URL', f"postgresql://{_pg('USER')}:{_secret()}@{_pg('HOST')}/{_pg('DB')}"
     )
     return ChainlitDataLayer(database_url=database_url)
 